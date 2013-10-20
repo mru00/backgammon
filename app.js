b82f42c12a26c3bc0098e3518b91a16be94dcb72
mru <mru@sisyphus.teil.cc>
2013-10-20 12:27:15 +0200

    changed listen config openshift

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ if(process.env.SUBDOMAIN){
 var app = express();
 
 app.configure(function(){
+  app.set('ipaddress', ip);
   app.set('port',  port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -48,7 +49,7 @@ app.get('/users', user.list);
 
 var server = http.createServer(app);
 
-server.listen(app.get('port'), function(){
+server.listen(app.get('port'), app.get('ipaddress'), function(){
   console.log("Express server listening on port " + app.get('port'));
   console.log(url);
 });
